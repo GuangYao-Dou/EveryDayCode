a7a467b8fd18f52de57056ebceebc34439013a37
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-07-22 10:27:31 +0800

    第一次提交

---
+++ src/main/java/com/dgy/LeetCode/Soulution.java
@@ -0,0 +1,10 @@
+package com.dgy.LeetCode;
+
+/**
+ * Date: 2019/6/30
+ * Time: 20:20
+ * Author: vincent-Dou
+ * Description：
+ */
+public class Soulution {
+}
---
+++ src/main/java/com/dgy/everyday/NiuKe0630.java
@@ -0,0 +1,22 @@
+package com.dgy.everyday;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+/**
+ * Date: 2019/6/30
+ * Time: 20:22
+ * Author: vincent-Dou
+ * Description：
+ */
+public class NiuKe0630 {
+//    static Map<Integer, String> map = new HashMap<>();
+//
+//    public static void main(String[] args) {
+//        map.put(1, "壹");
+//        map.put(2, "贰");
+//        map.put(3, "叁");
+//
+//    }
+}
