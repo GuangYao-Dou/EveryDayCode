f620572eebc23335f3939dfc0149b304e5295873
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-08-16 15:56:43 +0800

    快速排序

---
+++ src/main/java/com/dgy/sort/QuickSort.java
@@ -0,0 +1,51 @@
+package com.dgy.sort;
+
+/**
+ * Date: 2019/8/16
+ * Time: 15:35
+ * Author: vincent-Dou
+ * Description：快速排序
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = Utils.getRandomArr(20,10);
+        System.out.println("排序前：");
+        Utils.display(arr);
+        System.out.println("排序后：");
+        quickSort(arr,0, arr.length - 1);
+        Utils.display(arr);
+    }
+    public static void quickSort(int[] arr, int low, int high){
+        int i,j,temp,t;
+        if(low>high){
+            return;
+        }
+        i = low;
+        j=high;
+        //temp就是基准位
+        temp = arr[low];
+        while (i<j) {
+            //先看右边，依次往左递减
+            while (temp<=arr[j]&&i<j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp>=arr[i]&&i<j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i<j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        quickSort(arr, low, j-1);
+        //递归调用右半数组
+        quickSort(arr, j+1, high);
+    }
+}
