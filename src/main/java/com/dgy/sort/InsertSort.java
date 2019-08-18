519725ca894a5e9f86304652b1b2ec11150112e0
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-08-18 10:57:41 +0800

    插入排序

---
+++ src/main/java/com/dgy/sort/InsertSort.java
@@ -0,0 +1,33 @@
+package com.dgy.sort;
+import	java.time.Instant;
+
+/**
+ * Date: 2019/8/18
+ * Time: 10:11
+ * Author: vincent-Dou
+ * Description：
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = Utils.getRandomArr(20,10);
+        System.out.println("排序前：");
+        Utils.display(arr);
+        System.out.println("排序后");
+        insertSort(arr);
+        Utils.display(arr);
+    }
+    public static void insertSort(int[] arr){
+        if (arr == null || arr.length <= 1) return;
+        for (int i = 1; i < arr.length; i++){
+            int temp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--){
+                if (arr[j] <= temp) break;
+                if (arr[j] > temp){
+                    arr[j + 1] = arr[j];
+                }
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
