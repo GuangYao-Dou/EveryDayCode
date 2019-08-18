b4a43be66ffaedf8c3f4fdec487197fd0933c43c
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-08-18 16:43:03 +0800

    二叉树的翻转(二叉树的镜像)

--- src/main/java/com/dgy/NiuKe/NiuKe0818.java
+++ src/main/java/com/dgy/NiuKe/NiuKe0818.java
@@ -4,7 +4,22 @@ package com.dgy.NiuKe;
  * Date: 2019/8/18
  * Time: 16:31
  * Author: vincent-Dou
- * Description：
+ * Description：https://www.nowcoder.com/practice/564f4c26aa584921bc75623e48ca3011?tpId=13&tqId=11171&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
+ * 题目描述
+ * 操作给定的二叉树，将其变换为源二叉树的镜像。
+ * 输入描述:
+ * 二叉树的镜像定义：源二叉树
+ *     	    8
+ *     	   /  \
+ *     	  6   10
+ *     	 / \  / \
+ *     	5  7 9 11
+ *     	镜像二叉树
+ *     	    8
+ *     	   /  \
+ *     	  10   6
+ *     	 / \  / \
+ *     	11 9 7  5
  */
 public class NiuKe0818 {
     public void Mirror(TreeNode root) {
