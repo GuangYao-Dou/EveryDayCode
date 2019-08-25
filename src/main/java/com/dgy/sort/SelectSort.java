e4418e515945c613c4b38a3373d741c2cff02a8c
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-08-25 22:34:16 +0800

    选择排序

--- src/main/java/com/dgy/sort/SelectSort.java
+++ src/main/java/com/dgy/sort/SelectSort.java
@@ -13,10 +13,10 @@ import javax.rmi.CORBA.Util;
 public class SelectSort {
 
     public static void main(String[] args) {
-        int[] randomArr = Utils.getRandomArr(20, 100);
+        int[] randomArr = Utils.getRandomArr(20, 10);
         System.out.println("排序前");
         Utils.display(randomArr);
-        selectSort1(randomArr);
+        selectSort2(randomArr);
         System.out.println("排序后：");
         Utils.display(randomArr);
     }
@@ -41,6 +41,20 @@ public class SelectSort {
                 if (arr[index] > arr[j]) index = j;
             }
             int temp = arr [index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+
+    public static void selectSort2(int[] arr){
+        int index;
+        for (int i = 0; i < arr.length; i++) {
+            index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) index = j;
+            }
+            int temp = arr[index];
             arr[index] = arr[i];
             arr[i] = temp;
         }
