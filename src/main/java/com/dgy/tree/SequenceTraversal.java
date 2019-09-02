65d041962e86305be6075c2acc843e8e00848fa6
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-09-02 15:17:49 +0800

    二叉树的层序遍历

--- src/main/java/com/dgy/tree/SequenceTraversal.java
+++ src/main/java/com/dgy/tree/SequenceTraversal.java
@@ -1,6 +1,5 @@
 package com.dgy.tree;
 
-import javax.jnlp.ClipboardService;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,6 +17,26 @@ public class SequenceTraversal {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
         Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (count > 0){
+                root = queue.poll();
+                if (root.left != null) queue.offer(root.left);
+                if (root.right != null) queue.offer(root.right);
+                list.add(root.val);
+                count--;
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    public static List<List<Integer>> Sequence1(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<> ();
         queue.offer(root);
         while (!queue.isEmpty()){
             int count = queue.size();
