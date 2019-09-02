ccd367b885e3b033072a867d6c6c3b5d5ec20c03
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-09-02 14:43:40 +0800

    结点

---
+++ src/main/java/com/dgy/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package com.dgy.tree;
+
+/**
+ * Date: 2019/9/2
+ * Time: 14:42
+ * Author: vincent-Dou
+ * Description：
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
