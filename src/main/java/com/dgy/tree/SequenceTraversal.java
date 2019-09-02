684c6a44ec588306c2dc38f943f00dbb363a4e13
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-09-02 15:12:38 +0800

    二叉树的层序遍历

---
+++ src/main/java/com/dgy/tree/SequenceTraversal.java
@@ -0,0 +1,36 @@
+package com.dgy.tree;
+
+import javax.jnlp.ClipboardService;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Date: 2019/9/2
+ * Time: 15:04
+ * Author: vincent-Dou
+ * Description：二叉树的层序遍历
+ *https://leetcode-cn.com/problems/binary-tree-level-order-traversal/submissions/
+ */
+public class SequenceTraversal {
+    public static List<List<Integer>> Sequence(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (count > 0){
+                root = queue.poll();
+                if (root.left != null) queue.offer(root.left);
+                if (root.right != null) queue.offer(root.right);
+                list.add(root.val);
+                count--;
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
