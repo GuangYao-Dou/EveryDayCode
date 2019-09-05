360812cc057ee54276f47456a5c71ad9ce7f25b8
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-09-05 15:19:19 +0800

    构建链表节点

---
+++ src/main/java/com/dgy/everyday/ListNode.java
@@ -0,0 +1,13 @@
+package com.dgy.everyday;
+
+/**
+ * Date: 2019/9/5
+ * Time: 15:12
+ * Author: vincent-Dou
+ * Description：
+ */
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+}
