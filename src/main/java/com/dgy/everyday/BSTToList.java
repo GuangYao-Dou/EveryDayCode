210af4edc3de3472b2b2f0c6e0e1152e668ce0c6
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-09-15 19:45:10 +0800

    BST转换成双向链表

---
+++ src/main/java/com/dgy/everyday/BSTToList.java
@@ -0,0 +1,38 @@
+package com.dgy.everyday;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Date: 2019/9/15
+ * Time: 19:10
+ * Author: vincent-Dou
+ * Description：
+ */
+public class BSTToList {
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        if(pRootOfTree == null || (pRootOfTree.left == null && pRootOfTree.right == null)) return pRootOfTree;
+        List<TreeNode> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (pRootOfTree != null || !stack.isEmpty()){
+            if(pRootOfTree == null){
+                pRootOfTree = stack.pop();
+                list.add(pRootOfTree);
+                pRootOfTree = pRootOfTree.right;
+            }else {
+                stack.push(pRootOfTree);
+                pRootOfTree = pRootOfTree.left;
+            }
+        }
+        list.get(0).right = list.get(1);
+        list.get(0).left = null;
+        for (int i = 1; i < list.size() - 1; i++){
+            list.get(i).left = list.get(i - 1);
+            list.get(i).right = list.get(i + 1);
+        }
+        list.get(list.size() - 1).left = list.get(list.size() - 2);
+        list.get(list.size() - 1).right = null;
+        return list.get(0);
+    }
+}
---
+++ src/main/java/com/dgy/everyday/TreeNode.java
@@ -0,0 +1,16 @@
+package com.dgy.everyday;
+
+/**
+ * Date: 2019/9/15
+ * Time: 19:11
+ * Author: vincent-Dou
+ * Description：
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
