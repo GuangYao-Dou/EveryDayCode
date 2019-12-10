5d5b738130ade1eee3e940a87458df97b0ed6be6
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-12-10 17:24:39 +0800

    复习二叉树的后序遍历

--- src/main/java/com/dgy/tree/PostOrderTraversal.java
+++ src/main/java/com/dgy/tree/PostOrderTraversal.java
@@ -25,4 +25,18 @@ public class PostOrderTraversal {
         }
         return list;
     }
+
+    public static List<Integer> postOrder2(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            root = stack.pop();
+            if (root.left != null) stack.push(root.left);
+            if (root.right != null) stack.push(root.right);
+            list.add(0, root.val);
+        }
+        return list;
+    }
 }
