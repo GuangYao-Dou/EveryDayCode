fef168af498da165c03fc0e895abfe0d5aba31ea
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-12-13 16:50:24 +0800

    有序数组重建平衡二叉搜索树

---
+++ src/main/java/com/dgy/LeetCode/Solution108.java
@@ -0,0 +1,41 @@
+package com.dgy.LeetCode;
+
+/**
+ * Date: 2019/12/13
+ * Time: 16:44
+ * Author: vincent-Dou
+ * Description：
+ * 108. 将有序数组转换为二叉搜索树
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定有序数组: [-10,-3,0,5,9],
+ *
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ */
+public class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        // 左右等分建立左右子树，中间节点作为子树根节点，递归该过程
+        return nums == null ? null : buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildTree(nums, l, m - 1);
+        root.right = buildTree(nums, m + 1, r);
+        return root;
+    }
+}
